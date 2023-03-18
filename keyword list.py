8d1edb61bdc71eecf76b1dea14b53ad429a0b3e5
Ichiro <128242811+Ichiro-9@users.noreply.github.com>
2023-03-18 21:37:32 +0530

    Delete keyword list.py

--- keyword list.py
+++
@@ -1,9 +0,0 @@
-import os
-import socket
-
-hostname = socket.gethostname()
-ipaddr = str(socket.gethostbyname(hostname))
-IP = str("IP Address: ")
-print(hostname)
-print (str(IP) + str(ipaddr))
-print(os.getcwd())
